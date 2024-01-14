955a16b87efbf2d2e9b87bca792d06994eb6f114
Bear876 <124530493+Bear876@users.noreply.github.com>
2024-01-14 09:39:44 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDMCnU12UEgQ8vvqPEzDF6rWWM12Xh-DAQ",
+    authDomain: "chatverse-277d2.firebaseapp.com",
+    projectId: "chatverse-277d2",
+    storageBucket: "chatverse-277d2.appspot.com",
+    messagingSenderId: "726963304338",
+    appId: "1:726963304338:web:a039a58dcda23c505ff34b"
+  };
+  
+firebase.initializeApp(firebaseConfig);
